5c88196e426dc2e8963b21e02fe1a3388c41f82c
hendry <h3ndry@users.noreply.github.com>
2023-10-23 14:00:30 +0200

    feat(start): Initial commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SortingAlgrothm;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(Add(3,5));
+
---
+++ ConsoleApp/SortingAlgorithm.cs
@@ -0,0 +1,7 @@
+namespace SortingAlgrothm;
+
+public int Add(int a, int b)
+{
+    return a + b;
+}
+
