5dfe8fb9566c2b40c7c25669d181f2c311074d53
hendry <h3ndry@users.noreply.github.com>
2023-12-12 10:39:07 +0200

    refactor(add-test): add more common algo

--- ConsoleApp/SortingAlgorithm.cs
+++ ConsoleApp/SortingAlgorithm.cs
@@ -1,20 +1,37 @@
 public class SortingAlgorithm
 {
-    public void  SelectionSort(int[] input)
+    public void SelectionSort(int[] input)
     {
-        for (int i = 0; i < input.Length -1; i++)
+        for (int i = 0; i < input.Length - 1; i++)
         {
-            for (int j = i+ 1; j < input.Length; j++)
+            int index_sv = i;
+            for (int j = i + 1; j < input.Length; j++)
             {
-                if (input[i] > input[j]){
+                if (input[index_sv] > input[j])
+                {
+                    index_sv = j;
+                }
+            }
+            int temp = input[i];
+            input[i] = input[index_sv];
+            input[index_sv] = temp;
+        }
+    }
+
+    public void BubleSort(int[] input)
+    {
+        for (int i = 0; i < input.Length - 1; i++)
+        {
+            for (int j = i + 1; j < input.Length; j++)
+            {
+                if (input[i] > input[j])
+                {
                     int temp = input[i];
                     input[i] = input[j];
                     input[j] = temp;
-
                 }
             }
-
-        }
+       }
 
     }
 }
